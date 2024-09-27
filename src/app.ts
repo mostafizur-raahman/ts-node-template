441076ed0eb2ee3d304a9007716144ae84f5b5b8
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-09-27 22:20:11 +0600

    implement parser

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,23 @@ import express, { Request, Response } from "express"
 const app = express()
 export const port = 3000
 
+
+// middleware 
+app.use(express.json())
+
+
 app.get('/', (req: Request, res: Response) => {
     res.json('Hello World!')
 })
 
+app.post('/', (req: Request, res: Response) => {
+
+    const _doc = req.body;
+
+
+    return res.status(200).json({
+        message: "Hello world",
+        data: _doc
+    })
+})
 export default app;
