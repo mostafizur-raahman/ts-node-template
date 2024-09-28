9b6a93a5a9647af932ef73768697b9c7444446a5
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-09-28 15:56:26 +0600

    implement dotenv and mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,36 @@
-import express, { NextFunction, Request, Response } from "express"
-const app = express()
-export const port = 3000
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/modules/routes";
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
 
 
-// middleware 
-app.use(express.json())
+// routes 
+app.use("/api/v1", router)
 
-app.get('/', (req: Request, res: Response) => {
-    res.json('Hello World!')
-})
+app.get("/", (req: Request, res: Response) => {
+    res.json("Hello World!");
+});
 
-// global error handeler 
-
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+// global error handeler
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
     if (err) {
         return res.status(400).json({
             success: false,
-            message: "something is wrong."
-        })
+            message: "something is wrong.",
+        });
     }
-})
+});
 
 // not found
 app.all("*", (req: Request, res: Response) => {
     return res.status(404).json({
         success: false,
-        message: "Not found"
-    })
-})
-
-
+        message: "Not found",
+    });
+});
 
 export default app;
