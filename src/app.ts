99e4c25250c5a65583eca745a7fbbf0226a3fe2d
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-09-28 10:27:19 +0600

    feat: add global error handeler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 const app = express()
 export const port = 3000
 
@@ -11,14 +11,37 @@ app.get('/', (req: Request, res: Response) => {
     res.json('Hello World!')
 })
 
-app.post('/', (req: Request, res: Response) => {
+app.post('/api', (req: Request, res: Response, next: NextFunction) => {
 
-    const _doc = req.body;
+    try {
+        const _doc = req.body;
+
+        return res.status(200).json({
+            message: "Hello world",
+            data: _doc
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+
+// global error handeler 
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err) {
+        return res.status(400).json({
+            success: false,
+            message: "something is wrong."
+        })
+    }
+})
 
 
-    return res.status(200).json({
-        message: "Hello world",
-        data: _doc
+// not found
+app.all("*", (req: Request, res: Response) => {
+    return res.status(404).json({
+        success: false,
+        message: "Not found"
     })
 })
 export default app;
